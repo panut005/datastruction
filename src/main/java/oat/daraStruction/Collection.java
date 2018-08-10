edce8ce07c0f087582a46074d38858e367778e30
Oat <panutoat@gmail.com>
2018-08-10 14:34:58 +0700

    SE work1

---
+++ src/main/java/oat/daraStruction/Collection.java
@@ -0,0 +1,13 @@
+package oat.daraStruction;
+
+/**
+ * Created by 708 on 8/10/2018.
+ */
+public interface Collection {
+    public void add(Object element);
+    public void remove(Object element);
+    public boolean isEmpty();
+    public boolean contains(Object element);
+    public int size();
+
+}
